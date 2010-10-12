7901119a414300c4e2de42b1c7130ff5c0bd2cc7
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-12 15:59:06 +0200

    update ruby test server to return something

--- test/test_server.rb
+++ test/test_server.rb
@@ -1,14 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 get '/?' do
+  "GET succesful."
 end
 
 post '/?' do
+  params
 end
 
 put '/?' do
+  params
 end
 
 delete '/?' do
-
+  "DELETE succesful."
 end

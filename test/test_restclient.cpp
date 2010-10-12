9024d0793755a9f4fd18227eda4e945cd0d9909b
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-12 14:56:05 +0200

    add unit test suite with sinatra test server

---
+++ test/test_restclient.cpp
@@ -0,0 +1,55 @@
+#include "restclient.h"
+#include <gtest/gtest.h>
+#include <string>
+
+class RestClientTest : public ::testing::Test
+{
+ protected:
+    int foo;
+    std::string url;
+    std::string ctype;
+    std::string data;
+
+    RestClientTest()
+    {
+    }
+
+    virtual ~RestClientTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+      foo = 0;
+      url = "http://localhost:4567";
+      ctype = "";
+      data = "";
+    }
+
+    virtual void TearDown()
+    {
+    }
+
+};
+
+// Tests
+TEST_F(RestClientTest, TestRestClientGET)
+{
+    RestClient::get(url);
+    EXPECT_EQ(0, foo);
+}
+TEST_F(RestClientTest, TestRestClientPOST)
+{
+    RestClient::post(url, ctype, data);
+    EXPECT_EQ(0, foo);
+}
+TEST_F(RestClientTest, TestRestClientPUT)
+{
+    RestClient::put(url, ctype, data);
+    EXPECT_EQ(0, foo);
+}
+TEST_F(RestClientTest, TestRestClientDELETE)
+{
+    RestClient::del(url);
+    EXPECT_EQ(0, foo);
+}
---
+++ test/test_server.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'sinatra'
+get '/?' do
+end
+
+post '/?' do
+end
+
+put '/?' do
+end
+
+delete '/?' do
+
+end

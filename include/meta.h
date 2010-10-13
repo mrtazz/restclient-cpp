615e492f5990913f8d329d0c0f0c6e0873aebfe3
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-13 15:08:13 +0200

    add meta file

---
+++ include/meta.h
@@ -0,0 +1,18 @@
+/**
+ * @file meta.h
+ * @brief header file for meta information
+ * @author Daniel Schauenberg <d@unwiredcouch.com>
+ */
+
+#ifndef INCLUDE_META_H_
+#define INCLUDE_META_H_
+
+#include <string>
+
+namespace RestClientMeta {
+  /** version string */
+  const std::string version = "0.1.0";
+};
+
+#endif  // INCLUDE_META_H_
+

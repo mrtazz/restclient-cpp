6beedae140d2e01e942b98f326f9837b8bdcbab1
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-14 21:50:54 +0200

    add HTTP PUT method

--- source/restclient.cpp
+++ source/restclient.cpp
@@ -138,6 +138,59 @@ RestClient::response RestClient::put(const std::string& url,
 {
   /** create return struct */
   RestClient::response ret;
+  /** build content-type header string */
+  std::string ctype_header = "Content-Type: " + ctype;
+
+  /** initialize upload object */
+  RestClient::upload_object up_obj;
+  up_obj.data = data.c_str();
+  up_obj.length = data.size();
+
+  // use libcurl
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+  if (curl)
+  {
+    /** set user agent */
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, RestClient::user_agent);
+    /** set query URL */
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    /** Now specify we want to PUT data */
+    curl_easy_setopt(curl, CURLOPT_PUT, 1L);
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+    /** set read callback function */
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, RestClient::read_callback);
+    /** set data object to pass to callback function */
+    curl_easy_setopt(curl, CURLOPT_READDATA, &up_obj);
+    /** set callback function */
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, RestClient::write_callback);
+    /** set data object to pass to callback function */
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ret);
+    /** set data size */
+    curl_easy_setopt(curl, CURLOPT_INFILESIZE,
+                     static_cast<long>(up_obj.length));
+
+    /** set content-type header */
+    curl_slist* header = NULL;
+    header = curl_slist_append(header, ctype_header.c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
+    /** perform the actual query */
+    res = curl_easy_perform(curl);
+    if (res != 0)
+    {
+      std::cerr << "Failed to query " << url << ":"
+          << std::endl << curl_easy_strerror(res) << std::endl << std::flush;
+      exit(1);
+    }
+    long http_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    ret.code = static_cast<int>(http_code);
+
+    curl_easy_cleanup(curl);
+  }
+
   return ret;
 }
 /**

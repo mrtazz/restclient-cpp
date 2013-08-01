6ec6768ce6197049383ed22f6f854b57b05307f8
Daniel Schauenberg <dschauenberg@etsy.com>
2013-08-01 13:20:31 +0200

    use vendorized gtest

--- utils/build_gtest.sh
+++ utils/build_gtest.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-wget http://googletest.googlecode.com/files/gtest-1.6.0.zip
-unzip gtest-1.6.0.zip
-cd gtest-1.6.0
+cd vendor/gtest-1.6.0
 ./configure
 make
-sudo cp -a include/gtest /usr/include
-sudo cp -a lib/.libs/* /usr/lib/
-sudo ldconfig -v | grep gtest

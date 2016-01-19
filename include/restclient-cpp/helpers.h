c39ad6069db6d97dee242f5b31b08b16b5bf0507
Daniel Schauenberg <d@unwiredcouch.com>
2016-01-18 21:34:43 -0500

    exclude the trim functions from linting

--- include/restclient-cpp/helpers.h
+++ include/restclient-cpp/helpers.h
@@ -50,21 +50,21 @@ namespace Helpers {
                               void *userdata);
 
   // trim from start
-  static inline std::string &ltrim(std::string &s) {
+  static inline std::string &ltrim(std::string &s) {  // NOLINT
     s.erase(s.begin(), std::find_if(s.begin(), s.end(),
           std::not1(std::ptr_fun<int, int>(std::isspace))));
     return s;
   }
 
   // trim from end
-  static inline std::string &rtrim(std::string &s) {
+  static inline std::string &rtrim(std::string &s) { // NOLINT
     s.erase(std::find_if(s.rbegin(), s.rend(),
           std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
     return s;
   }
 
   // trim from both ends
-  static inline std::string &trim(std::string &s) {
+  static inline std::string &trim(std::string &s) {  // NOLINT
     return ltrim(rtrim(s));
   }
 };  // namespace Helpers

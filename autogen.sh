d49bc09cda69495c754cff9688ae09f5ae467d37
Giuseppe Ursino <giuseppe.ursino@vimar.com>
2019-01-02 14:12:55 +0100

    fix autogen to work with yocto

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-autoreconf --force --install
+#! /bin/sh
+[ -e config.cache ] && rm -f config.cache
+
+libtoolize --automake
+aclocal ${OECORE_ACLOCAL_OPTS}
+autoconf
+autoheader
+automake -a
+./configure $@
+exit
